33ee2ab48d9ae30295c7926cf1a241d8203491c1
Jean Pierre <jp0056392@gmail.com>
2026-05-18 15:48:42 -0300

    Criação das classes com requisitos

---
+++ Exercise-1/src/main/kotlin/model/Course.kt
@@ -0,0 +1,22 @@
+package org.example.model
+
+data class Course(
+    val id:Int,
+    val title:String,
+    val woarkloadHours:Int,
+    val level: CourseLevel
+){
+
+    init {
+        require (id>0){
+            "ID DEVE SER POSITIVO"
+        }
+
+        require (title!=null){
+            "CURSO DEVE HAVER UM TÍTULO"
+        }
+        require(woarkloadHours>0){
+            "A CARGA HORÁRIA DEVE SER MAIOR QUE 0"
+        }
+    }
+}
---
+++ Exercise-1/src/main/kotlin/model/CourseLevel.kt
@@ -0,0 +1,7 @@
+package org.example.model
+
+enum class CourseLevel {
+    BASIC,
+    INTERMEDIATE,
+    ADVANCED
+}
