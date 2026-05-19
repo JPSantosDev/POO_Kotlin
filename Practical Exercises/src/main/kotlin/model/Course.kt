7455e7e59e76dd0b2817120acee294e4c453b62e
Jean Pierre <jp0056392@gmail.com>
2026-05-19 14:32:03 -0300

    Fix de requires dentro das classes

--- Practical Exercises/src/main/kotlin/model/Course.kt
+++ Practical Exercises/src/main/kotlin/model/Course.kt
@@ -8,6 +8,18 @@ class Course(
 ){
     private val courses = mutableListOf<Course>()
 
+    init {
+        require (id>0){
+            "ID DEVE SER POSITIVO"
+        }
+        require (title!=null){
+            "CURSO DEVE HAVER UM TÍTULO VÁLIDO"
+        }
+        require(workloadHours>0){
+            "A CARGA HORÁRIA DEVE SER MAIOR QUE 0"
+        }
+    }
+
     fun summary(id: Int,title: String,woarkloadHours: Int,level: CourseLevel): String {
         return "O curso de $title com id $id tem $woarkloadHours horas de carga horária, com um nível ${level}"
     }
@@ -32,15 +44,4 @@ class Course(
         return courses.remove(course)
     }
 
-    init {
-        require (id>0){
-            "ID DEVE SER POSITIVO"
-        }
-        require (title!=null){
-            "CURSO DEVE HAVER UM TÍTULO VÁLIDO"
-        }
-        require(workloadHours>0){
-            "A CARGA HORÁRIA DEVE SER MAIOR QUE 0"
-        }
-    }
 }
--- Practical Exercises/src/main/kotlin/model/Trail.kt
+++ Practical Exercises/src/main/kotlin/model/Trail.kt
@@ -4,24 +4,32 @@ data class Trail(
     val id:Int,
     val name: String,
     val status: TrailStatus,
-)
-private val courseList = mutableListOf<Course>()
+) {
+    private val courseList = mutableListOf<Course>()
 
-fun addCourseToTrail(course: Course,status: TrailStatus):Boolean{
-    if(status != TrailStatus.OPEN)
-        return false
-    if(courseList.any{it.id == course.id})
-        return false
-    courseList.add(course)
-    return true
-}
+    init {
+        require(id>0) { "id da trilha deve ser positivo" }
+        require (name.isNotBlank()) { "trilha deve haver um nome" }
+    }
 
-fun removeCourseFromTrail(id:Int, status: TrailStatus):Boolean{
-    if(status != TrailStatus.OPEN)
-        return false
-    return courseList.removeIf { it.id == id }
+    fun addCourseToTrail(course: Course, status: TrailStatus): Boolean {
+        if (status != TrailStatus.OPEN)
+            return false
+        if (courseList.any { it.id == course.id })
+            return false
+        courseList.add(course)
+        return true
+    }
+
+    fun removeCourseFromTrail(id: Int, status: TrailStatus): Boolean {
+        if (status != TrailStatus.OPEN)
+            return false
+        return courseList.removeIf { it.id == id }
+    }
+
+    fun totalTrailWorkload(): Int {
+        return courseList.sumOf { it.workloadHours }
+    }
+
+    fun listCourses(): List<Course> = courseList.toList()
 }
-fun totalTrailWorkload(): Int{
-    return courseList.sumOf { it.workloadHours }
-}
-fun listCourses(): List<Course> = courseList.toList()
