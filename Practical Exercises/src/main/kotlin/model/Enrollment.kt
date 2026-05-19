ff9f903c30e15894c07487988722a1d5d6d43e43
Jean Pierre <jp0056392@gmail.com>
2026-05-19 15:14:28 -0300

    Assimilação de student com enrollment

--- Practical Exercises/src/main/kotlin/model/Enrollment.kt
+++ Practical Exercises/src/main/kotlin/model/Enrollment.kt
@@ -3,7 +3,15 @@ package org.example.model
 class Enrollment (
     val student: Student,
     val trail: Trail,
-    val status: EnrollmentStatus
+    status: EnrollmentStatus = EnrollmentStatus.ACTIVE
 ){
+    var status: EnrollmentStatus = status
+        private set
 
+    fun finish():Boolean{
+        if(status == EnrollmentStatus.CANCELLED)
+            return false
+        status = EnrollmentStatus.FINISHED
+        return true
+    }
 }
--- Practical Exercises/src/main/kotlin/model/EnrollmentStatus.kt
+++ Practical Exercises/src/main/kotlin/model/EnrollmentStatus.kt
@@ -2,6 +2,6 @@ package org.example.model
 
 enum class EnrollmentStatus {
     ACTIVE,
-    FINSHED,
+    FINISHED,
     CANCELLED
 }
