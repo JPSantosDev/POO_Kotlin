c4568cf6cefd421fcc16f8a8e16600ac125eb9d6
Jean Pierre <jp0056392@gmail.com>
2026-05-19 16:08:00 -0300

    Criação de métodos para ranking, classificação e porcentagem em StudentReport

--- Practical Exercises/src/main/kotlin/model/Course.kt
+++ Practical Exercises/src/main/kotlin/model/Course.kt
@@ -6,45 +6,48 @@ class Course(
     val workloadHours:Int,
     val level: CourseLevel,
     val status: TrailStatus
-){
+) {
     private val courses = mutableListOf<Course>()
 
     init {
-        require (id>0){
+        require(id > 0) {
             "ID DEVE SER POSITIVO"
         }
-        require (title!=null){
+        require(title != null) {
             "CURSO DEVE HAVER UM TÍTULO VÁLIDO"
         }
-        require(workloadHours>0){
+        require(workloadHours > 0) {
             "A CARGA HORÁRIA DEVE SER MAIOR QUE 0"
         }
     }
 
-    fun summary(id: Int,title: String,woarkloadHours: Int,level: CourseLevel): String {
+    fun summary(id: Int, title: String, woarkloadHours: Int, level: CourseLevel): String {
         return "O curso de $title com id $id tem $woarkloadHours horas de carga horária, com um nível ${level}"
     }
-    fun addCourse(course: Course):Boolean{
-        if(courses.any{it.id == course.id}) return false
+
+    fun addCourse(course: Course): Boolean {
+        if (courses.any { it.id == course.id }) return false
         courses.add(course)
         return true
     }
-    fun searchCourse(course:Course):Course?{
-        return courses.find{it.id == course.id}
+
+    fun searchCourse(course: Course): Course? {
+        return courses.find { it.id == course.id }
     }
-    fun orderCourse(): List<Course>{
-       return courses.sortedBy { it.title }
+
+    fun orderCourse(): List<Course> {
+        return courses.sortedBy { it.title }
     }
-    fun levelCourse(): List<Course>{
+
+    fun levelCourse(): List<Course> {
         return courses.filter { it.level == level }
     }
-    fun totalWorkload():Int{
+
+    fun totalWorkload(): Int {
         return courses.sumOf { it.workloadHours }
     }
-    fun removeCourse(course: Course): Boolean{
+
+    fun removeCourse(course: Course): Boolean {
         return courses.remove(course)
     }
-    fun totalCourses(): Int = courses.size
-
-    fun completedCourses(): List<Course> = courses.filter { it.status == TrailStatus.COMPLETED }
 }
--- Practical Exercises/src/main/kotlin/model/StudentReport.kt
+++ Practical Exercises/src/main/kotlin/model/StudentReport.kt
@@ -7,4 +7,28 @@ class StudentReport (
     val completedCourses:Int,
     val totalCourses:Int,
 
-)
+){
+    init {
+        require(completedCourses >= 0) { "Cursos completados deve ser um valor positivo" }
+        require(totalCourses > 0) { "Cursos totais deve ser maior que 0" }
+    }
+    fun percent():Int{
+        if(totalCourses == 0) return 0
+        return (completedCourses*100)/totalCourses
+    }
+    fun classification(): String{
+       return when(percent()){
+           0 -> "Sem prograsso"
+           in 1..79 -> "Em andamento"
+           in 80..99 -> "Quase concluido"
+           else -> "Concluido"
+       }
+    }
+
+    fun ranking(): List<Pair<String, Int>> {
+    val lista = mutableListOf<Pair<String,Int>>()
+        lista.add(Pair(student.name,percent()))
+        return lista.sortedWith(compareByDescending<Pair<String, Int>> { it.second } .thenBy { it.first })
+
+    }
+}
