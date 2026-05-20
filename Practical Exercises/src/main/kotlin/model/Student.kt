e5ea46f4950dc6526ed75981af40d48039a48483
Jean Pierre <jp0056392@gmail.com>
2026-05-20 14:34:28 -0300

    Mais fixes em student

--- Practical Exercises/src/main/kotlin/model/Student.kt
+++ Practical Exercises/src/main/kotlin/model/Student.kt
@@ -9,7 +9,7 @@ class Student(
         require(name.isNotEmpty()) { "Nome não pode estar vazio" }
         require(name.length >=3){"Nome deve haver pelo menos 3 letras" }
         require(email.isNotEmpty()){"Email não pode estar vazio"}
-        require(email.contains("@gmail.com")) { "Email deve conter '@gmail.com' "}
+        require(email.contains('@') && email.contains('.') && email.length>=3) { "Email deve conter '@gmail.com' "}
         require(id>0){"ID deve conter um numero positivo"}
     }
 }
